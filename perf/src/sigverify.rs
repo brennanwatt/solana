0c6bf50beb2710d8c89045cd6ba7e1940f91ed4c
Brennan Watt <brennan.watt@solana.com>
2022-06-01 17:31:15 -0700

    Update sigverify.rs

--- perf/src/sigverify.rs
+++ perf/src/sigverify.rs
@@ -596,11 +596,16 @@ pub fn ed25519_verify_cpu(batches: &mut [PacketBatch], reject_non_vote: bool, pa
                 .par_iter_mut()
                 .for_each(|p| verify_packet(p, reject_non_vote))
         });
-    });*/
+    });
     batches.into_iter().for_each(|batch| {
-        batch.into_iter().for_each(|p: &mut Packet| {
+        batch.for_each(|p: &mut Packet| {
             verify_packet(p, reject_non_vote)
         })
+    });*/
+    batches.into_par_iter().for_each(|batch| {
+        batch
+            .par_iter_mut()
+            .for_each(|p| verify_packet(p, reject_non_vote))
     });
     inc_new_counter_debug!("ed25519_verify_cpu", packet_count);
 }
