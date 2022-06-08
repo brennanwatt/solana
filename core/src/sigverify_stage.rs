6a7424136bc498912f3c70543cf558e7b6d68e82
Brennan Watt <brennan.watt@solana.com>
2022-06-08 07:34:04 -0700

    Update sigverify_stage.rs

--- core/src/sigverify_stage.rs
+++ core/src/sigverify_stage.rs
@@ -401,7 +401,7 @@ impl SigVerifyStage {
         stats.total_discard_time_us += discard_time.as_us() as usize;
         stats.total_verify_time_us += verify_time.as_us() as usize;
         stats.total_shrink_time_us += (pre_shrink_time_us + post_shrink_time_us) as usize;
-        stats.report(&"verify_iter".to_string());
+        stats.report("verify_iter");
 
         Ok(())
     }
