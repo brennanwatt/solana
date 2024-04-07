3b556b51575f01e35400013336b2645f7a2dc630
bw-solana <brennan.watt@anza.xyz>
2024-04-07 18:30:13 +0000

    more logs

--- core/src/banking_stage/unprocessed_transaction_storage.rs
+++ core/src/banking_stage/unprocessed_transaction_storage.rs
@@ -627,6 +627,11 @@ impl ThreadLocalUnprocessedPackets {
                             packets_to_process,
                             &mut total_tracer_packets_in_buffer,
                         );
+                    
+                    println!("#BW: forwarded_packets: {:?}, packets_to_forward: {:?}, tracers: {}",
+                        forwarded_packets.len(),
+                        packets_to_forward.len(),
+                        total_tracer_packets_in_buffer);
 
                     [
                         forwarded_packets,
@@ -777,6 +782,9 @@ impl ThreadLocalUnprocessedPackets {
 
         let filtered_count = packets_to_process.len().saturating_sub(transactions.len());
         saturating_add_assign!(*total_dropped_packets, filtered_count);
+        if filtered_count > 0 {
+            println!("#BW: filtered out {} packets", filtered_count);
+        }
 
         (transactions, transaction_to_packet_indexes)
     }
@@ -796,6 +804,9 @@ impl ThreadLocalUnprocessedPackets {
 
         let filtered_count = transactions.len().saturating_sub(results.len());
         saturating_add_assign!(*total_dropped_packets, filtered_count);
+        if filtered_count > 0 {
+            println!("#BW: filtered out {} invalid transactions", filtered_count);
+        }
 
         results
             .iter()
